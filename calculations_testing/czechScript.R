d4725120b23037d2da9ceed0d4bca2323e82f2a8
st55790 <tomas-o@seznam.cz>
2021-11-23 21:54:47 +0100

    add countries.csv for Chernoff faces, tests, anova

--- calculations_testing/czechScript.R
+++ calculations_testing/czechScript.R
@@ -75,33 +75,15 @@ kurtosis(data_czech$new_cases_smoothed_per_million)
 kurtosis(data_czech$hosp_patients, na.rm = TRUE)
 kurtosis(data_czech$hosp_patients_per_million, na.rm = TRUE)
 
-#https://cs.wikipedia.org/wiki/T-test#Jednov%C3%BDb%C4%9Brov%C3%BD_t-test
-
-t.test(data_czech$new_cases, mu = 3300)
-t.test(data_czech$new_cases_smoothed)
-t.test(data_czech$new_cases_per_million)
-t.test(data_czech$new_cases_smoothed_per_million)
-t.test(data_czech$hosp_patients)
-t.test(data_czech$hosp_patients_per_million)
-
-t.test(data_czech$new_cases_per_million, data_slovakia$new_cases_per_million, var.equal = TRUE)
-t.test(data_czech$new_cases_per_million, data_germany$new_cases_per_million, var.equal = TRUE)
-t.test(data_czech$new_cases_per_million, data_poland$new_cases_per_million, var.equal = TRUE)
-t.test(data_czech$new_cases_per_million, data_austria$new_cases_per_million, var.equal = TRUE)
-
 #https://www.dummies.com/education/math/statistics/base-r-statistical-functions/
 
-hist(data_czech$new_cases)
+#HISTOGRAMY
 hist(data_czech$new_cases_smoothed)
 hist(data_czech$new_cases_per_million)
 hist(data_czech$new_cases_smoothed_per_million)
 hist(data_czech$hosp_patients_per_million)
 hist(data_czech$new_cases, main = "Histogram: Hospitalizováni pacienti v ÈR",
      ,xlab = "Hospitalizovaní v CR", border = "red", col = "yellow",xlim = c(0,16000), las = 1, breaks = 8)
-
-
-
-#Grafy
 
 #Histogram novì nakažených
 histNewTests <- hist(log1p(data_czech$new_tests),
@@ -145,6 +127,7 @@ legend("topright",
        fill = c(rgb(0,0,1,1/4),rgb(1,0,0,1/4)),       # Color of the squares
        border = "black") # Color of the border of the squares
 
+#PLOTY
 b <- plot(log1p(data_czech$new_cases), pch = 4)
 a <- plot(data_czech$new_tests, xlim = c(180,600),pch = 4)
 plot(data_czech$reproduction_rate, pch = 4)
@@ -157,12 +140,13 @@ plot(data_czech$new_vaccinations, pch = 4)
 plot(data_czech$excess_mortality, pch = 4)
 plot(log1p(data_czech$new_cases), pch = 4)
 
+#BOXPLOTY
 boxplot(data_czech$new_cases_per_million)
 boxplot(data_czech$reproduction_rate)
 boxplot(log1p(data_czech$new_deaths),
         main = "Cesko zlogaritmovane nove smrti",
 )
-
+#3D GRAFY
 library("plot3D")
 scatter3D(data_czech$new_cases, data_czech$new_tests, data_czech$total_cases)
 scatter3D(log1p(data_czech$new_cases), log1p(data_czech$new_tests), log1p(data_czech$total_cases))
@@ -171,6 +155,7 @@ scatter3D(data_czech$new_cases, data_czech$new_tests, data_czech$new_vaccination
 scatter3D(c(1:576), c(1:576), log1p(data_czech$total_cases))
 scatter3D(c(1:576), c(1:576), (data_czech$reproduction_rate))
 
+#HEXBIN
 library(hexbin)
 library(RColorBrewer)
 
@@ -178,6 +163,76 @@ bin <- hexbin(log1p(data_czech$new_cases), data_czech$new_deaths, xbins = 40)
 my_colors = colorRampPalette(rev(brewer.pal(11,'Spectral')))
 plot(bin, main = "" , colramp=my_colors , legend = F )
 
+#CHERNOFF FACES
 library(aplpack)
-tabulka <- table(c(1:10), c(1:10))
-faces(tabulka)
+countries <- c("Czech", "Slovakia", "Germany", "Poland", "Austria")
+countriesMeanNewCases <- c(mean(data_czech$new_cases),
+                             mean(data_slovakia$new_cases),
+                             mean(data_germany$new_cases),
+                             mean(data_poland$new_cases),
+                             mean(data_austria$new_cases))
+
+countriesMeanTotalCases <- c(mean(data_czech$total_cases),
+                             mean(data_slovakia$total_cases),
+                             mean(data_germany$total_cases),
+                             mean(data_poland$total_cases),
+                             mean(data_austria$total_cases))
+
+countriesPopulation <- c(mean(data_czech$population),
+                             mean(data_slovakia$population),
+                             mean(data_germany$population),
+                             mean(data_poland$population),
+                             mean(data_austria$population))
+
+tabulka <- data.frame(countries, countriesMeanNewCases, countriesMeanTotalCases, countriesPopulation)
+
+faces(tabulka[,-1], labels = tabulka[,1])
+
+##QQPLOT
+?qqplot()
+qqplot(data_czech$new_cases,data_czech$new_deaths)
+qqplot(data_czech$new_tests,data_czech$new_cases)
+
+#TESTOVANI HYPOTEZ
+#TESTY
+#https://cs.wikipedia.org/wiki/T-test#Jednov%C3%BDb%C4%9Brov%C3%BD_t-test
+
+#JEDNOVYBEROVY STUDENTUV TEST
+t.test(data_czech$new_cases, mu = 3300)
+t.test(data_czech$new_cases_smoothed)
+t.test(data_czech$new_cases_per_million)
+t.test(data_czech$new_cases_smoothed_per_million)
+t.test(data_czech$hosp_patients)
+t.test(data_czech$hosp_patients_per_million)
+
+#DVOUVYBEROVY STUDENTUV TEST
+p1 = data_czech$new_cases[1:288]
+p2 = data_czech$new_cases[289:576]
+t.test(p1,p2)
+t.test(data_czech$new_cases_per_million, data_slovakia$new_cases_per_million, var.equal = FALSE)
+t.test(data_czech$new_cases_per_million, data_germany$new_cases_per_million, var.equal = TRUE)
+t.test(data_czech$new_cases_per_million, data_poland$new_cases_per_million, var.equal = TRUE)
+t.test(data_czech$new_cases_per_million, data_austria$new_cases_per_million, var.equal = TRUE)
+
+#WILCOX TEST
+wilcox.test(data_czech$new_cases_per_million, data_slovakia$new_cases_per_million)
+wilcox.test(data_czech$new_cases_per_million, data_germany$new_cases_per_million)
+
+#FISHERUV TEST
+var.test(data_czech$new_cases_per_million, data_slovakia$new_cases_per_million)
+var.test(lm(data_czech$new_cases_per_million ~ 1), lm(data_slovakia$new_cases_per_million ~ 1)) #stejny jak to nad tim
+
+#SHAPIRO WILK TEST
+shapiro.test(data_czech$new_cases)
+shapiro.test(data_czech$new_tests)
+
+#ANOVA
+anova <- aov(data_czech$new_tests~data_czech$new_cases+data_czech$new_deaths)
+summary(anova)
+par(mfrow = c(3,2))
+plot(anova, 1)
+plot(anova, 2)
+plot(anova, 3)
+plot(anova, 4)
+plot(anova, 5)
+plot(anova, 6)
